fcf182a0cd9ed61399267104125ea856f2d6e4b6
zenithtekla <zenithtekla@gmail.com>
2016-09-08 07:35:35 -0700

    r3 step 4 add non-func form in ng2-meteor

--- client/app.component.ts
+++ client/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 
 import { DemoComponent } from './imports/demo/demo.component';
 import { Parties } from '../both/collections/parties.collection';
+import { PartiesFormComponent } from './imports/parties/parties-form.component';
 
 import template from './app.component.html';
 
 @Component({
   selector: 'app',
   template,
-  directives: [DemoComponent]
+  directives: [DemoComponent, PartiesFormComponent]
 })
 export class AppComponent {
   parties: Mongo.Cursor<any>;
