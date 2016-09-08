ade4bac7570c565ceafdd33b1425faf927e2d1c4
zenithtekla <zenithtekla@gmail.com>
2016-09-08 07:27:13 -0700

    r2 step 3 add Mongo & fixtures data

--- client/main.ts
+++ client/main.ts
@@ -1,3 +1,5 @@
+
+
 import { bootstrap } from 'angular2-meteor-auto-bootstrap';
 
 import { AppComponent } from './app.component';
--- server/main.ts
+++ server/main.ts
@@ -1,4 +1,18 @@
+/*
 import { Main } from './imports/server-main/main';
 
 let mainInstance = new Main();
 mainInstance.start();
+*/
+
+import { Main } from './imports/server-main/main';
+import {loadParties} from './imports/fixtures/parties';
+import {Meteor} from 'meteor/meteor';
+
+let mainInstance = new Main();
+mainInstance.start();
+Meteor.startup(() => {
+  // load initial Parties
+  loadParties();
+});
+
